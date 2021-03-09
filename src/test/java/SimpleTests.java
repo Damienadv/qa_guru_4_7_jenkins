ec6c3e2b4e98c7c9928333fb3e2a832e270edad9
login <andropovdv@gmail.com>
2021-03-09 10:50:14 +0300

    added hamcrest

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,18 +1,19 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class SimpleTests {
 
     @Test
     void successTest() {
-        assertEquals(true, true);
+        assertThat(true, is(true));
 
     }
 
     @Test
     void negativeTest() {
-        assertEquals(true, false);
+        assertThat(true, is(false));
 
     }
 }
